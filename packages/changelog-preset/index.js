f20990707344013d92799e1f4b9748f1259aa3d8
Bean <bean@yuanfen.net>
2023-10-20 16:54:26 +0800

    feat: edit recommendedBumpOpts

--- packages/changelog-preset/index.js
+++ packages/changelog-preset/index.js
@@ -1,7 +1,9 @@
 const createPrest = require('conventional-changelog-conventionalcommits')
+const { createConventionalRecommendedBumpOpts } = require('./conventionalRecommendedBump.js')
 
 module.exports = async function createPreset(config) {
   const preset = await createPrest(config)
   preset.parserOpts.headerPattern = /^(.*?)(?:\(([\w$.\-*/ ]*)\))?: (.*)$/
+  preset.recommendedBumpOpts = createConventionalRecommendedBumpOpts(config, preset.parserOptspreset.parserOpts)
   return preset
 }
