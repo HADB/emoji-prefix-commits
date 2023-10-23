baf6e1400c4cffd305aac43dd6519877beb64ec8
Bean <bean@yuanfen.net>
2023-10-23 10:46:06 +0800

    feat: copy source code

---
+++ packages/changelog-preset/constants.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const DEFAULT_COMMIT_TYPES = Object.freeze([
+  { type: '✨feat', section: 'Features' },
+  { type: '🐛fix', section: 'Bug Fixes' },
+  { type: '🚀perf', section: 'Performance Improvements' },
+  { type: '⏪revert', section: 'Reverts' },
+  { type: '📚docs', section: 'Documentation', hidden: true },
+  { type: '🎨style', section: 'Styles', hidden: true },
+  { type: '🔧chore', section: 'Miscellaneous Chores', hidden: true },
+  { type: '🦄refactor', section: 'Code Refactoring', hidden: true },
+  { type: '✅test', section: 'Tests', hidden: true },
+  { type: '📦build', section: 'Build System', hidden: true },
+  { type: '👷ci', section: 'Continuous Integration', hidden: true }
+].map(Object.freeze))
+
+exports.DEFAULT_COMMIT_TYPES = DEFAULT_COMMIT_TYPES
--- packages/changelog-preset/index.js
+++ packages/changelog-preset/index.js
@@ -1,59 +1,29 @@
-const createPrest = require('conventional-changelog-conventionalcommits')
-const breakingHeaderPattern = /^(\w*)(?:\((.*)\))?!: (.*)$/
+'use strict'
 
-function addBangNotes(commit) {
-  const match = commit.header.match(breakingHeaderPattern)
-  if (match && commit.notes.length === 0) {
-    const noteText = match[3] // the description of the change.
-    commit.notes.push({
-      text: noteText,
-    })
-  }
-}
+const { DEFAULT_COMMIT_TYPES } = require('./constants')
+const { createParserOpts } = require('./parserOpts')
+const { createWriterOpts } = require('./writerOpts')
+const { createConventionalChangelogOpts } = require('./conventionalChangelog')
+const { createConventionalRecommendedBumpOpts } = require('./conventionalRecommendedBump')
 
-function createConventionalRecommendedBumpOpts(config, parserOpts) {
+async function createPreset (config) {
+  const parserOpts = createParserOpts(config)
+  const writerOpts = await createWriterOpts(config)
+  const recommendedBumpOpts = createConventionalRecommendedBumpOpts(config, parserOpts)
+  const conventionalChangelog = createConventionalChangelogOpts(parserOpts, writerOpts)
+
   return {
-    parserOpts,
-
-    whatBump(commits) {
-      let level = 2
-      let breakings = 0
-      let features = 0
-
-      commits.forEach((commit) => {
-        // adds additional breaking change notes
-        // for the special case, test(system)!: hello world, where there is
-        // a '!' but no 'BREAKING CHANGE' in body:
-        addBangNotes(commit)
-        if (commit.notes.length > 0) {
-          breakings += commit.notes.length
-          level = 0
-        } else if (commit.type === '✨feat' || commit.type === 'feat') {
-          features += 1
-          if (level === 2) {
-            level = 1
-          }
-        }
-      })
-
-      if (config?.preMajor && level < 2) {
-        level++
-      }
-
-      return {
-        level,
-        reason:
-          breakings === 1
-            ? `There is ${breakings} BREAKING CHANGE and ${features} features`
-            : `There are ${breakings} BREAKING CHANGES and ${features} features`,
-      }
+    gitRawCommitsOpts: {
+      ignore: config?.ignoreCommits,
+      noMerges: null
     },
+    parserOpts,
+    writerOpts,
+    recommendedBumpOpts,
+    conventionalChangelog
   }
 }
 
-module.exports = async function createPreset(config) {
-  const preset = await createPrest(config)
-  preset.parserOpts.headerPattern = /^(.*?)(?:\(([\w$.\-*/ ]*)\))?: (.*)$/
-  preset.recommendedBumpOpts = createConventionalRecommendedBumpOpts(config, preset.parserOpts)
-  return preset
-}
+module.exports = createPreset
+
+module.exports.DEFAULT_COMMIT_TYPES = DEFAULT_COMMIT_TYPES
